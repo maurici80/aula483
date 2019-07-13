beda56dd78adca253b9f87f971b473bee8e7e7ec
Maurici Schmitz <maurici.schmitz@hotmail.com>
2019-07-13 16:06:52 -0300

    Segunda interacao com o GitHub

---
+++ SegundoContatocomGitHub/SegundoContatocomGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoContatocomGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Segundo projeto de interacao com o github
+            Console.WriteLine("Agora ja sei usar o basico do github");
+        }
+    }
+}
