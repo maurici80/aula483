f08daa199eea5f279b44062a59b7e3a87d356d56
Maurici Schmitz <maurici.schmitz@hotmail.com>
2019-07-13 19:30:55 -0300

    Trabaçhando com valores Async com Task

---
+++ AcessandoDadosDoBancoAsync/AcessandoDadosDoBancoAsync/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using AcessandoDadosDoBancoAsync.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcessandoDadosDoBancoAsync
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        DbContents contex = new DbContents();
+
+        List<User> usuarios = new List<User>();
+
+        DateTime time = new DateTime();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Task.Run(() =>{
+                while (true)
+                    time = DateTime.Now;
+            });
+                           //CallTeacher();
+        }
+
+
+        public async void CallTeacher()
+        {
+            usuarios = await Task.Run(() => usuarios = (from s in contex.Users
+                                                                   where s.Id > 0
+                                                                   select s).ToList<User>());
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //foreach (var item in usuarios)
+            //  MessageBox.Show(item.Usuario);
+
+            MessageBox.Show(time.ToString("hh:mm:ss.fff"));
+            
+        }
+    }
+}
