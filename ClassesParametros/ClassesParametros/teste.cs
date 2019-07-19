45b59e38a02b3716233a2289153038244be64fd0
Maurici Schmitz <maurici.schmitz@hotmail.com>
2019-07-19 05:19:32 -0300

    video 30 - Joao Ribeiro

---
+++ ClassesParametros/ClassesParametros/teste.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesParametros
+{
+    class teste
+    {
+
+        public int Acrescentar(int  x, int v = 10)
+        {
+            v = v + x;
+            return v;
+        }
+        public void Diminuir(ref int valor)
+        {
+            valor -= 5;
+        }
+    }
+}
+
+/* primeira implementacao
+        public void Acrescentar(ref int valor)
+        {
+            valor += 5;
+        }
+        public void Diminuir(ref int valor)
+        {
+            valor -= 5;
+        }
+*/
