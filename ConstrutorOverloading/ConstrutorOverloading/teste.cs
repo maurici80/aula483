f4f3f4924f7c08037084bbc7d3c1956092d33fc5
Maurici Schmitz <maurici.schmitz@hotmail.com>
2019-07-23 05:12:54 -0300

    Aula 32 - Joao Ribeiro

---
+++ ConstrutorOverloading/ConstrutorOverloading/teste.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConstrutorOverloading
+{
+    class teste
+    {
+
+        string _nome;
+        int _idade;
+
+        //construtor
+        public teste (string nome, int idade)
+        {
+            _nome = nome;
+            _idade = idade;
+        }
+
+        public void Apresentar()
+        {
+            MessageBox.Show(_nome + " -> " + _idade);
+        }
+        public void Apresentar(string separador)
+        {
+            MessageBox.Show(_nome + separador + _idade);
+        }
+        public void Apresentar(int idade)
+        {
+            MessageBox.Show(_nome + " .. " + + idade);
+        }
+
+
+    }
+}
