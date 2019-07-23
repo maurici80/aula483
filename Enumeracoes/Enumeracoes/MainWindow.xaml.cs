a951f723f6ca53aea49ee49795a72e5f9c3a15c7
Maurici Schmitz <maurici.schmitz@hotmail.com>
2019-07-23 05:37:57 -0300

    video 33 - joao ribeiro

---
+++ Enumeracoes/Enumeracoes/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Enumeracoes
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        teste objeto = new teste();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Console.WriteLine("Esta frase vai aparecer no console");
+            //teste objeto = new teste();
+            objeto.ApresentarDia(teste.DiasDaSemana.sábado);
+            objeto.Mover(teste.Movimentos.Direita);
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            objeto.Mover(teste.Movimentos.Cima);
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            objeto.Mover(teste.Movimentos.Esquerda);
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            objeto.Mover(teste.Movimentos.Baixo);
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            objeto.Mover(teste.Movimentos.Direita);
+        }
+    }
+}
---
+++ Enumeracoes/Enumeracoes/teste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeracoes
+{
+    class teste
+    {
+        public enum DiasDaSemana
+        {
+            domingo, segunda, terça, quarta, quinta, sexta, sábado
+
+        }
+
+        public enum Movimentos { Cima, Baixo, Esquerda, Direita}
+        public void ApresentarDia(DiasDaSemana dia)
+        {
+            Console.WriteLine("O dia escolhido foi "+ dia);
+        }
+
+        public void Mover(Movimentos direcao)
+        {
+            Console.WriteLine("O jogador moveu-se para " + direcao);
+        }
+    }
+}
